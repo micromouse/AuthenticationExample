eb68706b8d0fb25b6819141c5546e98c186d6109
micromouse <eggeggak@vip.qq.com>
2023-05-31 17:57:53 +0800

    first commit

---
+++ CookieAuthenticatioin/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using AuthenticationExample.CookieAuthenticatioin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+using System.Diagnostics;
+
+namespace AuthenticationExample.CookieAuthenticatioin.Controllers {
+    /// <summary>
+    /// Home控制器
+    /// </summary>
+    public class HomeController : Controller {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IDiagnosticContext _diagnosticContext;
+
+        /// <summary>
+        /// 初始化Home控制器
+        /// </summary>
+        /// <param name="logger">日之前</param>
+        /// <param name="diagnosticContext">During request processing, additional properties can be attached to the completion event using IDiagnosticContext.Set()</param>
+        public HomeController(ILogger<HomeController> logger, IDiagnosticContext diagnosticContext) {
+            _logger = logger;
+            _diagnosticContext = diagnosticContext;
+        }
+
+        public IActionResult Index() {
+            _diagnosticContext.Set("Action", "HomeController.Index");
+            return View();
+        }
+
+        public IActionResult Privacy() {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error() {
+            var model = new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier };
+            _logger.LogError("系统发生了未处理错误:{@ErrorViewModel}", args: model);
+            return View(model);
+        }
+    }
+}
---
+++ CookieAuthenticatioin/Program.cs
@@ -0,0 +1,89 @@
+using Serilog;
+using Serilog.Events;
+using Serilog.Formatting.Compact;
+
+namespace AuthenticationExample.CookieAuthenticatioin {
+    /// <summary>
+    /// Cookie认证
+    /// </summary>
+    public class Program {
+        /// <summary>
+        /// main
+        /// </summary>
+        /// <param name="args">参数</param>
+        public static void Main(string[] args) {
+            //配置Serilog
+            ConfigureSerilog();
+
+            try {
+                Log.Information("Starting web application");
+
+                var builder = WebApplication.CreateBuilder(args);
+
+                //配置Serilog
+                builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext()
+                    .WriteTo.Console());
+                
+                // Add services to the container.                
+                builder.Services.AddControllersWithViews();
+
+                var app = builder.Build();
+
+                app.UseSerilogRequestLogging(options =>
+                {
+                    // Customize the message template
+                    //options.MessageTemplate = "[{RequestMethod}-{StatusCode}-{Elapsed}]Handled {RequestScheme}//{RequestHost}{RequestPath}";
+
+                    // Emit debug-level events instead of the defaults
+                    //options.GetLevel = (httpContext, elapsed, ex) => LogEventLevel.Information;
+
+                    // Attach additional properties to the request completion event
+                    options.EnrichDiagnosticContext = (diagnosticContext, httpContext) =>
+                    {
+                        diagnosticContext.Set("RequestHost", httpContext.Request.Host.Value);
+                        diagnosticContext.Set("RequestScheme", httpContext.Request.Scheme);
+                    };
+                });
+
+                // Configure the HTTP request pipeline.
+                if (!app.Environment.IsDevelopment()) {
+                    app.UseExceptionHandler("/Home/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseHttpsRedirection();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+                app.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                app.Run();
+            } catch (Exception ex) {
+                Log.Fatal(ex, "Application terminated unexpectedly");
+            } finally {
+                Log.CloseAndFlush();
+            }
+
+        }
+
+        /// <summary>
+        /// 配置Serilog
+        /// </summary>
+        static void ConfigureSerilog() {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Debug)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .CreateBootstrapLogger();
+        }
+    }
+}
